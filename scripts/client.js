d059da32c6e86285fcdc5ba251efad7b00ce889c
Norman Khine <khinester@aqoon.local>
2012-10-23 17:27:20 +0100

    now the canvas scales and added jquery-ui for overlays

--- scripts/client.js
+++ scripts/client.js
@@ -3,6 +3,7 @@ function asisiumClient() {
   var iconPath = "m 1.4194515,-160.64247 c 33.5874165,0 60.8159465,-25.97005 60.8159465,-58.00952 0,-32.0404 -27.22755,-58.0114 -60.8159465,-58.0114 -33.5883965,0 -60.8159415,25.971 -60.8159415,58.0114 0,32.0404 27.228527,58.00952 60.8159415,58.00952 z m 81.9575765,26.25762 C 70.531608,-146.64352 55.269688,-153.983 0.08110256,-153.983 c -55.19742156,0 -70.08915856,7.96609 -82.28062656,19.59815 -12.197359,11.62926 -8.081167,135.7024419 -8.081167,135.7024419 L -63.292733,-59.848397 -46.325227,122.37766 2.6291765,29.116913 48.308878,122.37766 64.467298,-59.848397 91.457218,1.3175919 c 0,-8e-4 4.76917,-123.4484419 -8.08019,-135.7024419 z";
   var width = $("#vis").width(),
     mapCanvasHeight = (width * 0.45);
+
   this.init = function() {
 
     self.loadCopyright();
@@ -30,7 +31,8 @@ function asisiumClient() {
     
     console.log(width + " " + mapCanvasHeight);
     // Most parts of D3 don"t know anything about SVG—only DOM.
-    self.svg = d3.select("#vis").append("svg:svg").attr("width", "100%").attr("height", "100%")
+    self.svg = d3.select("#vis").append("svg:svg").attr("width", "100%").attr("height", "100%").attr("viewBox", "0 0 " + width + " " + mapCanvasHeight);
+
     // Load data from .json file
     //self.svg.selectAll("circle")
     //    .data(dataset)
@@ -46,8 +48,8 @@ function asisiumClient() {
       .style("stroke", "gray")
       .style("fill", "white")
       .attr("r", 10)
-      .attr("cx", 300)
-      .attr("cy", 20)
+      .attr("cx", 320)
+      .attr("cy", 25)
       .on("mouseover", function(){d3.select(this).style("fill", "red");})
       .on("mouseout", function(){d3.select(this).style("fill", "white");});
 
@@ -55,11 +57,29 @@ function asisiumClient() {
       .style("stroke", "gray")
       .style("fill", "white")
       .attr("r", 10)
-      .attr("cx", 400)
-      .attr("cy", 10)
+      .attr("cx", 520)
+      .attr("cy", 0)
       .on("mouseover", function(){d3.select(this).style("fill", "red");})
       .on("mouseout", function(){d3.select(this).style("fill", "white");});
       
+    self.svg.append("circle")
+      .style("stroke", "gray")
+      .style("fill", "white")
+      .attr("r", 10)
+      .attr("cx", 730)
+      .attr("cy", 5)
+      .on("mouseover", function(){d3.select(this).style("fill", "red");})
+      .on("mouseout", function(){d3.select(this).style("fill", "white");});
+
+    self.svg.append("circle")
+      .style("stroke", "gray")
+      .style("fill", "white")
+      .attr("r", 10)
+      .attr("cx", 900)
+      .attr("cy", 30)
+      .on("mouseover", function(){d3.select(this).style("fill", "red");})
+      .on("mouseout", function(){d3.select(this).style("fill", "white");});
+
   
   };
 	// Initialise
