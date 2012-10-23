8fb6fb9b66ff4cad7698b3d3e4f7d57942f8a34a
Norman Khine <khinester@aqoon.local>
2012-10-23 18:21:57 +0100

    work on icon path

--- scripts/client.js
+++ scripts/client.js
@@ -1,6 +1,6 @@
 function asisiumClient() {
   var self = this;
-  var iconPath = "m 1.4194515,-160.64247 c 33.5874165,0 60.8159465,-25.97005 60.8159465,-58.00952 0,-32.0404 -27.22755,-58.0114 -60.8159465,-58.0114 -33.5883965,0 -60.8159415,25.971 -60.8159415,58.0114 0,32.0404 27.228527,58.00952 60.8159415,58.00952 z m 81.9575765,26.25762 C 70.531608,-146.64352 55.269688,-153.983 0.08110256,-153.983 c -55.19742156,0 -70.08915856,7.96609 -82.28062656,19.59815 -12.197359,11.62926 -8.081167,135.7024419 -8.081167,135.7024419 L -63.292733,-59.848397 -46.325227,122.37766 2.6291765,29.116913 48.308878,122.37766 64.467298,-59.848397 91.457218,1.3175919 c 0,-8e-4 4.76917,-123.4484419 -8.08019,-135.7024419 z";
+  var iconPath = "M 10,28 C 4.4771525,28 0,32.477153 0,38 c 0,5.522847 4.4771525,10 10,10 5.522848,0 10,-4.477153 10,-10 l 0,-0.09115 C 19.946619,32.424236 15.484878,28 10,28 z m 0,2.708333 c 3.99939,0 7.252743,3.227363 7.291667,7.226563 l 0,0.0651 c 0,4.027076 -3.264591,7.291667 -7.291667,7.291667 -4.0270763,0 -7.2916667,-3.264591 -7.2916667,-7.291667 0,-4.027076 3.2645904,-7.291667 7.2916667,-7.291667 z";
   var width = $("#vis").width(),
     mapCanvasHeight = (width * 0.45);
 
@@ -43,15 +43,15 @@ function asisiumClient() {
     //    .attr("width", 75)
     //    .attr("x", function(d, i){return i*80})
     //    .attr("y", 20);
-        
-    self.svg.append("circle")
-      .style("stroke", "gray")
-      .style("fill", "white")
-      .attr("r", 10)
-      .attr("cx", 320)
-      .attr("cy", 25)
-      .on("mouseover", function(){d3.select(this).style("fill", "red");})
-      .on("mouseout", function(){d3.select(this).style("fill", "white");});
+    var icon = self.svg.append("svg:path");
+    icon.attr("d", iconPath);
+    icon.attr("transform", "translate(" + 310 + "," + 20 + ")")
+    icon.style("fill", "white");
+    icon.attr("class", "member");
+    icon.on("mouseover", function(){d3.select(this).style("fill", "red");});
+    icon.on("mouseout", function(){d3.select(this).style("fill", "white");});
+
+
 
     self.svg.append("circle")
       .style("stroke", "gray")
