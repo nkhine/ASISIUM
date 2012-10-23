af21c0698d7b134fe86e5051392668d509f76de3
Norman Khine <khinester@aqoon.local>
2012-10-23 18:53:46 +0100

    added the icon with text

--- scripts/client.js
+++ scripts/client.js
@@ -1,6 +1,6 @@
 function asisiumClient() {
   var self = this;
-  var iconPath = "M 10,28 C 4.4771525,28 0,32.477153 0,38 c 0,5.522847 4.4771525,10 10,10 5.522848,0 10,-4.477153 10,-10 l 0,-0.09115 C 19.946619,32.424236 15.484878,28 10,28 z m 0,2.708333 c 3.99939,0 7.252743,3.227363 7.291667,7.226563 l 0,0.0651 c 0,4.027076 -3.264591,7.291667 -7.291667,7.291667 -4.0270763,0 -7.2916667,-3.264591 -7.2916667,-7.291667 0,-4.027076 3.2645904,-7.291667 7.2916667,-7.291667 z";
+  var iconPath = "M 10,28 C 4.4771525,28 0,32.477153 0,38 c 0,5.522847 4.4771525,10 10,10 5.522848,0 10,-4.477153 10,-10 l 0,-0.09115 C 19.946619,32.424236 15.484878,28 10,28 z m 0,4.791667 c 2.856707,0 5.18053,2.299678 5.208333,5.15625 l 0,0.05208 c 0,2.876483 -2.33185,5.208333 -5.208333,5.208333 -2.8764829,0 -5.2083333,-2.33185 -5.2083333,-5.208333 0,-2.876483 2.3318504,-5.208333 5.2083333,-5.208333 z";
   var width = $("#vis").width(),
     mapCanvasHeight = (width * 0.45);
 
@@ -47,10 +47,16 @@ function asisiumClient() {
     icon.attr("d", iconPath);
     icon.attr("transform", "translate(" + 310 + "," + 20 + ")")
     icon.style("fill", "white");
-    icon.attr("class", "member");
+    icon.attr("class", "icon");
     icon.on("mouseover", function(){d3.select(this).style("fill", "red");});
     icon.on("mouseout", function(){d3.select(this).style("fill", "white");});
 
+    var iconName = self.svg.append("svg:text");
+    iconName.text('About')
+    iconName.style("fill", "white")
+    iconName.attr("transform", "translate(" + 320 + "," + 45 + ")")
+    iconName.attr('text-anchor', 'middle')
+    iconName.attr("class", "icon-name");
 
 
     self.svg.append("circle")
