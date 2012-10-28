c6f33fabfbcec5869573cc248ff74a852aaa1b95
Norman Khine <khinester@aqoon.local>
2012-10-28 15:14:34 +0000

    removed the contact form and added the background gradiant to the dialog boxes from http://www.colorzilla.com/gradient-editor/

--- scripts/client.js
+++ scripts/client.js
@@ -140,33 +140,11 @@ function asisiumClient() {
 
   this.contact = function() {
     $( "#contact-form" ).dialog({ 
-      autoOpen: true, 
-      height: 450, 
-      width: 500, 
-      modal: true, 
-      buttons: { 
-        "Send": function() { 
-          var bValid = true; 
-          allFields.removeClass( "ui-state-error" );
-          bValid = bValid && checkLength( email, "email", 6, 80 );
-          bValid = bValid && checkRegexp( email, /^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i, "eg. ui@jquery.com" ); 
-         
-          if ( bValid ) { 
-            $( "#messages tbody" ).append( "<tr>" + 
-            "<td>" + name.val() + "</td>" +  
-            "<td>" + email.val() + "</td>" +  
-            "<td>" + massage.val() + "</td>" + 
-            "</tr>" );  
-            $( this ).dialog( "close" ); 
-          } 
-        }, 
-        Cancel: function() { 
-          $( this ).dialog( "close" ); 
-        } 
-      }, 
-      close: function() { 
-        allFields.val( "" ).removeClass( "ui-state-error" ); 
-      } 
+      autoOpen: true,
+      height: 370,
+      width: 900,
+      modal: true,
+      position: ['center', 150],
     });
   };
   // Initialise  
@@ -177,5 +155,6 @@ function asisiumClient() {
 var asisiumClient;
 
 jQuery(function() {
-	asisiumClient = new asisiumClient();
+  asisiumClient = new asisiumClient();
+  $(".ui-dialog-titlebar").hide();
 });
