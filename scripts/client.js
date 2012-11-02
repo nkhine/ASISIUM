893e36f3ecfa66833785721553f5f239df3ad4f2
Norman Khine <khinester@aqoon.local>
2012-11-02 13:58:47 +0000

    fix click to close dialog

--- scripts/client.js
+++ scripts/client.js
@@ -3,8 +3,11 @@ function asisiumClient() {
   var iconPath = "M 0.14285728,7.9999996 C -5.3799908,7.9999996 -9.8571425,12.477152 -9.8571425,18 c 0,5.522847 4.4771517,10 9.99999978,10 C 5.6657051,28 10.142857,23.522847 10.142857,18 l 0,-0.09115 C 10.089477,12.424235 5.6277351,7.9999996 0.14285728,7.9999996 z m 0,4.7916664 c 2.85670682,0 5.18052982,2.299679 5.20833282,5.156251 l 0,0.05208 c 0,2.876483 -2.33185,5.208333 -5.20833282,5.208333 -2.87648308,0 -5.20833308,-2.33185 -5.20833308,-5.208333 0,-2.876483 2.33185,-5.208334 5.20833308,-5.208334 z";
   var width = $("#map").width(),
     mapCanvasHeight = (width * 0.38);
-  
-  var items = ['ABOUT', 'SERVICES', 'PROJECTS', 'CONTACT']
+
+  var data = [{name:'about', x: 308, y: 89}, 
+              {name:'services', x: 533, y: 49}, 
+              {name:'projects', x: 752, y: 16}, 
+              {name:'contact', x: 915, y: 77}];
   
   this.init = function() {
     self.svg = d3.select("#map").append("svg:svg")
@@ -12,6 +15,7 @@ function asisiumClient() {
       //.attr("viewBox", "0 0 " + width + " " + mapCanvasHeight);
 
     self.loadCopyright();
+    //self.drawMarkers();
     self.drawAbout();
     self.drawServices();
     self.drawProjects();
@@ -27,7 +31,22 @@ function asisiumClient() {
       x.innerHTML='copyright ' + '&copy; ' + 'ASISIUM ' + d.getFullYear();
     }
   };
-  
+
+  // draw markers from data
+  this.drawMarkers = function () {
+    console.log(data);
+    //var icon = self.svg.append('svg:g')
+    //  .data(data)
+    //  .enter().append('rect')
+    //    .attr('x', x)
+    //    .attr('y', y)
+    //    .attr('width', 20)
+    //    .attr('height', 20)
+    //    .style('fill', 'steelblue');
+    //
+    
+  };
+  // we draw each marker - TODO - optimise this to use a list
   this.drawAbout = function () {
     var iconHolder = self.svg.append('svg:g')
       .attr("transform", "translate(" + 308 + "," + 89 + ")")
@@ -111,31 +130,47 @@ function asisiumClient() {
 
   // Overlay to display the content for each section
   this.about = function() {
-    $('.about').dialog({
+    $('#about').dialog({
       autoOpen: true,
       height: 350,
       width: 400,
       modal: true,
       position: [130, 220],
+      open: function(){
+        jQuery('.ui-widget-overlay').bind('click',function(){
+          jQuery('#about').dialog('close');
+        })
+      }
     });
   };
   this.services = function() {
-    $('.services').dialog({
+    $('#services').dialog({
       autoOpen: true,
       height: 350,
       width: 400,
       modal: true,
       position: [350, 190],
+      open: function(){
+        jQuery('.ui-widget-overlay').bind('click',function(){
+          jQuery('#services').dialog('close');
+        })
+      }
     });
   };
   this.projects = function() {
-    $('.projects').dialog({
+    $('#projects').dialog({
       autoOpen: true,
       height: 370,
       width: 655,
       modal: true,
       position: [450, 160],
+      open: function(){
+        jQuery('.ui-widget-overlay').bind('click',function(){
+          jQuery('#projects').dialog('close');
+        })
+      }
     });
+    $(".ui-dialog").addClass("customclass");
     $("#tabs").tabs().removeClass('ui-widget-content');
   };
 
@@ -146,6 +181,11 @@ function asisiumClient() {
       width: 400,
       modal: true,
       position: [720, 210],
+      open: function(){
+        jQuery('.ui-widget-overlay').bind('click',function(){
+          jQuery('#contact-form').dialog('close');
+        })
+      }
     });
   };
   // Initialise  
